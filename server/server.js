8b606031feb433f3f447586ba863b58c6ce7f824
wasiq2004 <104295043+wasiq2004@users.noreply.github.com>
2025-11-22 13:29:43 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -72,6 +72,9 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // ------------------------------------------------
  // For Twilio webhook form data
 
